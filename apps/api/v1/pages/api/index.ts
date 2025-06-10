30a36ffe3e5d67fa62661de4285133d69297f69a
Code Bot Test <support@codebottest.com>
2025-06-10 18:16:29 +0000

    refactor: integrate middleware into CalcomApi handler

--- apps/api/v1/pages/api/index.ts
+++ apps/api/v1/pages/api/index.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { withMiddleware } from "~/lib/helpers/withMiddleware";
 
-export default async function CalcomApi(_: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Welcome to Cal.com API - docs are at https://developer.cal.com/api" });
-}
+export default withMiddleware()(
+  async function CalcomApi(_: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({ message: "Welcome to Cal.com API - docs are at https://developer.cal.com/api" });
+  }
+);
